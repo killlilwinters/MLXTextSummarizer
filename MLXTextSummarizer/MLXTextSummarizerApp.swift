a25e658a3e3dfec7c5421a44fc1349b08d50d25d
MaksGorobets <gorobets542@gmail.com>
2025-03-19 14:17:14 +0200

    Initial Commit

---
+++ MLXTextSummarizer/MLXTextSummarizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLXTextSummarizerApp.swift
+//  MLXTextSummarizer
+//
+//  Created by Maks Winters on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MLXTextSummarizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
